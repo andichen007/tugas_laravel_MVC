6f9dcf6ae97a4d74ed6d065f2876d852d8d281d2
andichen007 <67626898+andichen007@users.noreply.github.com>
2020-07-01 07:38:44 +0700

    tugasLaravel1

---
+++ resources/views/form.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORM_Andi Muhamad Yusup</title>
+</head>
+<body>
+
+	<h1>Buat Account Baru!</h1>
+
+	<h3>Sign Up Form</h3>
+
+	<form action="/welcome" method="get">
+		<p><label>First Name :</label></p>
+		<p><input type="text"></p>
+
+		<p><label>Last Name :</label></p>
+		<p><input type="text"></p>
+
+		<p><label>Gender :</label></p>
+		<input type="radio" name="gender" value="0" checked>Male <br>
+		<input type="radio" name="gender" value="1">Female <br>
+		<input type="radio" name="gender" value="2">Other <br>
+
+		<p><label>Nationality :</label></p>
+		<select>
+			<OPTION>Indonesia</OPTION>
+			<OPTION>Korea</OPTION>
+			<OPTION>India</OPTION>
+		</select>
+
+		<p><label>Language Spoken :</label></p>
+		<input type="checkbox" name="bahasa" value="0"> Indonesia <br>
+		<input type="checkbox" name="bahasa" value="1"> English <br>	
+		<input type="checkbox" name="bahasa" value="2"> Other <br>	
+
+		<p><label>Bio :</label></p>
+		<p><textarea cols=25 rows="4" ></textarea></p>
+		<input type="submit" value="Sign Up">
+	</form>
+
+</body>
+</html>
